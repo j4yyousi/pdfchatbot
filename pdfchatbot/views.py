bf3fe059f5ca06c4bb2729b0b2d574e047a104d2
Khaled ALJayyousi <khaledj4y@gmail.com>
2025-09-12 19:23:06 +0300

    make test_get_homepage pass

--- pdfchatbot/views.py
+++ pdfchatbot/views.py
@@ -2,5 +2,5 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 def home(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>Hello World</h1>")
+    return HttpResponse("<title>PDF Q/A Chatbot</title>")
 
