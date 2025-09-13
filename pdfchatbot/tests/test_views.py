ef93f5c142c119b604e069eb413fa1bd08ce7738
Khaled ALJayyousi <khaledj4y@gmail.com>
2025-09-13 10:45:08 +0300

    write test to check if upload_pdf_form_exists.

--- pdfchatbot/tests/test_views.py
+++ pdfchatbot/tests/test_views.py
@@ -3,30 +3,38 @@ from django.http import HttpResponse
 from bs4 import BeautifulSoup, Tag
 from bs4.element import PageElement
 from typing import Optional
+from django.urls import reverse
+
 OK = 200
 
 class HomeViewTest(TestCase):
     def setUp(self) -> None:
-        pass
+        self.http_response: HttpResponse = self.client.get(reverse('home'))
+        self.response = BeautifulSoup(self.http_response.content, "html.parser")
     
     def test_get_homepage(self):
-        http_response: HttpResponse = self.client.get("/")
-        self.assertEqual(http_response.status_code, OK)
-        response = BeautifulSoup(http_response.content, "html.parser")
+        self.assertEqual(self.http_response.status_code, OK)
         #below code can be replaced by a one liner but I don't want a runtime error
         #if title is None and string is accessed. Also to get rid of pylance errors.
-        title: Optional[Tag] = response.title
+        title: Optional[Tag] = self.response.title
         if isinstance(title, Tag):
             self.assertEqual("PDF Q&A Chatbot", title.string)
         else:
             self.fail("title not found")
 
+    def test_upload_pdf_form_exists(self) -> None:
+        form_elem: Optional[PageElement] = self.response.find("form", {"id": "upload-form"})
+        if isinstance(form_elem, Tag):
+            self.assertEqual(form_elem.get("method"), "post")
+            self.assertEqual(form_elem.get("enctype"), "multipart/form-data")
+            self.assertEqual(form_elem.get("action"), reverse('home'))
+        else:
+            self.fail("form_elem not found")
+
     def test_upload_pdf_button_exists(self):
-        http_response: HttpResponse = self.client.get("/")
-        response = BeautifulSoup(http_response.content, "html.parser")
-        # input_elem is either a PageElement or None
-        input_elem: Optional[PageElement] = response.find("input", {"id": "pdf-file"})
-        self.assertIsNotNone(input_elem)
+        # input_elem is either a PageElement or None.
+        input_elem: Optional[PageElement] = self.response.find("input", {"id": "pdf-file"})
+        #A tag is a subclass of PageElement.
         if isinstance(input_elem, Tag):
             self.assertEqual(input_elem.get("type"), "file")
             self.assertEqual(input_elem.get("name"), "pdf_file")
