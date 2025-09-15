0a1a1fd0449e699f4daf8f393dd44a843b218f91
Khaled ALJayyousi <khaledj4y@gmail.com>
2025-09-15 17:35:31 +0300

    Enable CSS in html template

--- pdfchatbot/settings.py
+++ pdfchatbot/settings.py
@@ -114,9 +114,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "static" # after collection, files are placed here. Put this in .gitignore and run collectstatic in Docker.
+STATIC_URL = "static/" 
+STATICFILES_DIRS = [ # files are collected from these directories
+    BASE_DIR / "pdfchatbot/static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
